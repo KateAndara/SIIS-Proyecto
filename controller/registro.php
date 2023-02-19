6751f290e718319a505a72675d60b6afbf0c4b24
Deiby-Lainez <90879436+Deiby-Lainez@users.noreply.github.com>
2023-02-19 16:55:38 -0600

    actualización registro

--- controller/registro.php
+++ controller/registro.php
@@ -2,7 +2,7 @@
  
   
   if (!empty($_POST["btnRegistrar"])){
-       if (empty($_POST["Usuario"]) or empty($_POST["Clave"]) or empty($_POST["Confirmacion"])) {
+       if (empty($_POST["Usuario"]) or empty($_POST["Clave"]) or empty($_POST["Confirmacion"]) or empty($_POST["Email"])) {
            echo '<div class="alert alert-danger">Uno de los campos esta vacio</div> ';
        } elseif ($_POST["Clave"] != $_POST["Confirmacion"]){
            echo '<div class="alert alert-danger">Los campos de contraseña no coinciden</div> ';
@@ -12,19 +12,49 @@
         if ($datos=$sql->fetch_object()){
             echo '<div class="alert alert-danger">Este nombre de usuario ya existe</div> ';
         }else{
-            $rol=1;  // Rol agregado manualmente a la base de datos porque todavia no ejecuto una funcion para primer usuario
-            $cargo=1; // Cargo agregado manualmente a la bd porque todavia no ejecuto una funcion para primer usuario
+
+            $rol="DEFAULT";  
+            $cargo="Empleado";
+            //Condicion para crear el rol por defecto de los usuarios o selecionarlo
+            $sql1=$conexion->query(" select * from tbl_ms_roles where Rol='$rol' ");
+            if ($datos=$sql1->fetch_object()){
+              $sql2=$conexion->query(" select Id_Rol from tbl_ms_roles where Rol='$rol' ");
+              $id_rol=mysqli_fetch_assoc($sql2); // Id del rol en la BD
+              $id_rol= (int) $id_rol;
+            } else {
+              $sql2=$conexion -> query("insert into tbl_ms_roles(Rol,Descripcion)values('$rol','Rol por defecto')");
+              $sql3=$conexion->query(" select Id_Rol from tbl_ms_roles where Rol='$rol' ");
+              $id_rol=mysqli_fetch_assoc($sql3);  // Id del rol en la BD
+              $id_rol= (int) $id_rol;
+            }
+            //Condicion para crear el cargo por defecto de los usuarios o selecionarlo
+            $sql4=$conexion->query(" select * from tbl_cargos where Nombre_Cargo='$cargo' ");
+            if ($datos=$sql4->fetch_object()){
+              $sql5=$conexion->query(" select Id_Cargo from tbl_cargos where Nombre_Cargo='$cargo' ");
+              $id_cargo=mysqli_fetch_assoc($sql5); // Id del cargo en la BD
+              $id_cargo=(int)$id_cargo;
+              
+            } else {
+              $sql5=$conexion -> query("insert into tbl_cargos(Nombre_Cargo)values('$cargo')");
+              $sql6=$conexion->query(" select Id_Cargo from tbl_cargos where Nombre_Cargo='$cargo' ");
+              $id_cargo=mysqli_fetch_assoc($sql6); // Id del cargo en la BD
+              $id_cargo=(int)$id_cargo;
+            }
+
             $usuario=$_POST["Usuario"];
             $contraseña=$_POST["Clave"];
             $correo=$_POST["Email"];
             $Fecha=date("Y-m-d");
             $estado="Nuevo";
-            $sql=$conexion -> query("insert into tbl_ms_usuarios(Id_Rol,Id_Cargo,Usuario,Estado,Contraseña,Correo_Electronico, Fecha_creacion)values('$rol','$cargo','$usuario','$estado','$contraseña','$correo','$Fecha')");
+            $sql=$conexion -> query("insert into tbl_ms_usuarios(Id_Rol,Id_Cargo,Usuario,Estado,Contraseña,Correo_Electronico, Fecha_creacion)values('$id_rol','$id_cargo','$usuario','$estado','$contraseña','$correo','$Fecha')");
             if ($sql==1){
-                echo '<div class="alert alert-success">Usuario registrado correctamente</div> ';         
+                echo '<div class="alert alert-success">Usuario registrado correctamente</div> ';
+              
               } else {
                 echo '<div class="alert alert-danger">Error al ingresar al usuario</div> ';
               }
+
+            
         }
     
       } 
